44d47d690d2a276de211fe6bec372c618e2cec2c
JoYee03 <joyeelaw03@gmail.com>
2025-06-28 16:33:44 +0800

    Update

--- upload-image.ps1
+++ upload-image.ps1
@@ -7,7 +7,9 @@ $body = @{
     filename = "image/2025/Blank.jpg"
 } | ConvertTo-Json -Depth 3 -Compress
 
-Invoke-WebRequest -Uri "https://image-service-development-735683043266.asia-southeast1.run.app/testImageUpload" `
+$response = Invoke-WebRequest -Uri "https://image-service-development-735683043266.asia-southeast1.run.app/testImageUpload" `
   -Method POST `
   -Body $body `
   -ContentType "application/json"
+
+  $response.Content
